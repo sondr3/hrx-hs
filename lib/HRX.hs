61ff11b422531bfa6a30dcd53f8e2c16b1d05ebf
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-11-26 20:54:08 +0100

    Add parse function to public interface

--- lib/HRX.hs
+++ lib/HRX.hs
@@ -1,3 +1,11 @@
-module HRX () where
+module HRX (parse) where
 
-import HRX.Internal ()
+import Data.Text (Text)
+import HRX.Internal (Archive, ParserError, pArchive)
+import Text.Megaparsec (ParseErrorBundle)
+import qualified Text.Megaparsec as M
+
+parse :: FilePath -> Text -> Either (ParseErrorBundle Text ParserError) Archive
+parse path input = case M.parse pArchive path input of
+  Right archive -> Right archive
+  Left err -> Left err
