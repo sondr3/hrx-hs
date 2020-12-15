f93e7f0b461485e499c1aa222de1b1926da1f500
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-12-15 21:06:14 +0100

    Add exports to library

--- lib/HRX.hs
+++ lib/HRX.hs
@@ -1,8 +1,27 @@
 module HRX
-  ( readArchive,
+  ( -- * Data structures
+    Archive (..),
+    Entry (..),
+    EntryType (..),
+    Path (..),
+
+    -- * IO functions to read/write archives and entries
+    readArchive,
     writeArchive,
+
+    -- * Serialization functions to/from text
+    fromHRX,
     toHRX,
   )
 where
 
-import HRX.Internal (readArchive, toHRX, writeArchive)
+import HRX.Internal
+  ( Archive (..),
+    Entry (..),
+    EntryType (..),
+    Path (..),
+    fromHRX,
+    readArchive,
+    toHRX,
+    writeArchive,
+  )
