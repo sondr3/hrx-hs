a357cd046ad36b034c6844c912c2064bfef142d1
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-12-16 18:05:16 +0100

    Add doctests test-suite

---
+++ test/doctests.hs
@@ -0,0 +1,14 @@
+module Main where
+
+import Test.DocTest
+
+main :: IO ()
+main =
+  doctest
+    [ "-XNamedFieldPuns",
+      "-XOverloadedStrings",
+      "-isrc",
+      "lib/Codec/Archive/HRX.hs",
+      "lib/Codec/Archive/HRX/Internal.hs",
+      "lib/Codec/Archive/HRX/Parser.hs"
+    ]
